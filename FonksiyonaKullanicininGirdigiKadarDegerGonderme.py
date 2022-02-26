b8f398914b21af5070b3cf9e8e6e5be6dde29fcb
Muhammed Hukumdar <86476907+woosnick@users.noreply.github.com>
2022-02-26 12:50:13 +0300

    Add files via upload

---
+++ FonksiyonaKullanicininGirdigiKadarDegerGonderme.py
@@ -0,0 +1,5 @@
+def toplama(*degerler):#yildizin amaci istedigimiz kadar deger gonderebilmek
+    return sum(degerler)#sum fonksiyonu hazir toplama fonksiyonudur
+
+
+print(toplama(10,20,40,30))
---
+++ FonksiyondaKullanicininGirdigiKadarDegeriSozlukYapma.py
@@ -0,0 +1,5 @@
+def example_func(**kwargs):
+    print(kwargs)
+
+example_func(run=100,swim=200,basketball=300)
+example_func(a=2,b=5)
